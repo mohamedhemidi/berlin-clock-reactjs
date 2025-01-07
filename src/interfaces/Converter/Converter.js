24d4a4fa47a3c20190dd5570b026ad0d7795e531
mohamedhemidi <m.hamidi571@gmail.com>
2025-01-07 19:44:34 +0100

    added learning tab

--- src/interfaces/Converter/Converter.js
+++ src/interfaces/Converter/Converter.js
@@ -1,8 +1,14 @@
-import { useState, useEffect } from 'react';
-import { ClockBody } from '../../components';
-import {DigitalToBerlinConverter, BerlinToDigitalConverter} from '../../components';
-import { convertBerlinTimeToDigital, convertDigitalToBerlinTime } from '../../utils/helper';
-import './style.scss';
+import { useEffect, useState } from "react";
+import {
+  BerlinToDigitalConverter,
+  ClockBody,
+  DigitalToBerlinConverter,
+} from "../../components";
+import {
+  convertBerlinTimeToDigital,
+  convertDigitalToBerlinTime,
+} from "../../utils/helper";
+import "./style.scss";
 
 const Converter = () => {
   const [ToggleState, setToggleState] = useState(1);
@@ -11,21 +17,20 @@ const Converter = () => {
     setToggleState(index);
   };
 
-  const [time, setDate] = useState(new Date().toLocaleTimeString('fr-FR'));
+  const [time, setDate] = useState(new Date().toLocaleTimeString("fr-FR"));
 
   // const body = convertBerlinTimeToDigital(convertDigitalToBerlinTime(time));
 
   function refreshClock() {
-      setDate(new Date().toLocaleTimeString('fr-FR'));
+    setDate(new Date().toLocaleTimeString("fr-FR"));
   }
 
   useEffect(() => {
-      const timerId = setInterval(refreshClock, 1000);
-      return function cleanup() {
-          clearInterval(timerId);
-      };
+    const timerId = setInterval(refreshClock, 1000);
+    return function cleanup() {
+      clearInterval(timerId);
+    };
   }, []);
-
 
   const getActiveClass = (index, className) =>
     ToggleState === index ? className : "";
@@ -50,21 +55,46 @@ const Converter = () => {
         >
           Convert Digital Time to Berlin Time
         </li>
+        <li
+          className={`tabs ${getActiveClass(4, "active-tabs")}`}
+          onClick={() => toggleTab(4)}
+        >
+          Learn
+        </li>
       </ul>
       <div className="content-container">
         <div className={`content ${getActiveClass(1, "active-content")}`}>
-        <h2>{new Date().toLocaleTimeString('fr-FR')}</h2>
-          <ClockBody body={convertBerlinTimeToDigital(convertDigitalToBerlinTime(time))} />
+          <h2>{new Date().toLocaleTimeString("fr-FR")}</h2>
+          <ClockBody
+            body={convertBerlinTimeToDigital(convertDigitalToBerlinTime(time))}
+          />
         </div>
         <div className={`content ${getActiveClass(2, "active-content")}`}>
-         <BerlinToDigitalConverter body={convertBerlinTimeToDigital(convertDigitalToBerlinTime("00:00:01"))} time="00:00:01"/>
+          <BerlinToDigitalConverter
+            body={convertBerlinTimeToDigital(
+              convertDigitalToBerlinTime("00:00:01")
+            )}
+            time="00:00:01"
+          />
         </div>
         <div className={`content ${getActiveClass(3, "active-content")}`}>
-        <DigitalToBerlinConverter />
+          <DigitalToBerlinConverter />
+        </div>
+        <div className={`content ${getActiveClass(4, "active-content")}`}>
+          <h1>Learning</h1>
+          <iframe
+            height="560"
+            src="https://www.youtube.com/embed/SgrjEpEWO7k?si=nXiykoVl-B6wVtNa"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerpolicy="strict-origin-when-cross-origin"
+            allowfullscreen
+          ></iframe>
         </div>
       </div>
     </div>
   );
 };
 
-export default Converter
+export default Converter;
